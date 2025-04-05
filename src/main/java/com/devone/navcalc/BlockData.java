13cd9e5cbb44c36ab0377efdd69ea518ec27ffc8
Madckoy <madckoy@gmail.com>
2025-04-05 23:06:54 +0200

    Remove unnecessary Zone.Identifier files and update data file reference in config.properties; enhance air type checking in BlockData class

--- src/main/java/com/devone/navcalc/BlockData.java
+++ src/main/java/com/devone/navcalc/BlockData.java
@@ -1,15 +1,21 @@
 package com.devone.navcalc;
 
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+
 public class BlockData {
+
+    private static final Set<String> AIR_TYPES = Set.of("AIR", "CAVE_AIR", "VOID_AIR");
+
     public int x, y, z;
     public String type;
     public boolean bot;  // из JSON
 
     public boolean isAir() {
-        return "AIR".equalsIgnoreCase(type);
+        return type != null && AIR_TYPES.contains(type.toUpperCase());
     }
 
     public boolean isBot() {
